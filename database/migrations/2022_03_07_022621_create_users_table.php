3fd05ebde3d5510edccc9afccb2ed03c635d6682
ykpark <read0more@gmail.com>
2022-03-08 13:53:18 +0900

    Users 테이블 수정

--- database/migrations/2022_03_07_022621_create_users_table.php
+++ database/migrations/2022_03_07_022621_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('username')->unique();
+            $table->string('password');
             $table->string('bio')->nullable();
             $table->string('image')->nullable();
         });
