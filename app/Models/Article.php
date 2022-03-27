b77c2a53946fbf574a02a5183c28430b65428ffc
yk <read0more@gmail.com>
2022-03-27 20:37:01 +0900

    Add return timestamp when create article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -15,9 +16,33 @@ class Article extends Model
         'title', 'description', 'body'
     ];
 
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
     protected $attributes = [
         // 'tagList' => [],
     ];
+
+    // todo: 직접 바꾸는 식이 아니라 전체 필드에 대해서 camelcase로 바꾸는 방법 있는지 확인 필요
+    protected $appends = ['createdAt', 'updatedAt'];
+
+    public function getCreatedAtAttribute($key)
+    {
+        return $this->attributes['created_at'] ?? null;
+    }
+
+    public function getUpdatedAtAttribute($key)
+    {
+        return $this->attributes['updated_at'] ?? null;
+    }
+
 
     public function tags()
     {
